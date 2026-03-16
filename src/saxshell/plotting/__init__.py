93f9fb37f1449d67615fe4b69ac04322b45fa699
Keith White <keith.white@colorado.edu>
2026-03-16 16:55:53 -0600

    adding saxshell file structure for sub-application folders

---
+++ src/saxshell/plotting/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2026 The University of Colorado Boulder in the City of Boulder.
+# All rights reserved.
+#
+# File coded by: Keith White.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/kewh5868/saxshell/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+"""Python package for analysis of small-angle scattering data from molecular dynamics derived liquid structures."""
+
+# package version
+from saxshell.version import __version__  # noqa
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
